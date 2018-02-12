08e327929abc3652d09c90b881578d0950d62bfb
Pilar Rodriguez <celticbard077@gmail.com>
2018-02-12 02:28:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ if (name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected the name to be Susan")
+}if (index.height === 70) {
+    console.log("The height is correct")
+} else {
+    console.log("Expected: 70, Received: "+index.height)
 }
 
 
